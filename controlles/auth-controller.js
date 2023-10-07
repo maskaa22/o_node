cefc625c8ba5c8049af12eb98d3d128d199cf00a
maskaa22 <kyrosika565@gmail.com>
2023-10-07 14:41:22 +0300

    cookie changes _v6

--- controlles/auth-controller.js
+++ controlles/auth-controller.js
@@ -71,7 +71,8 @@ module.exports = {
             res.cookie(constantsConfig.REFRESH_TOKEN, tokenPair.refresh_token, {
                 maxAge: 30 * 24 * 60 * 60 * 1000,
                 httpOnly: true,
-                domain: '.o-node.onrender.com'
+                domain: '.o-node.onrender.com',
+                sameSite: 'none'
             });
             
 
